e91aa95d367131e5d7b3025c2469711186134c6b
darth-xiaozhijun <15717061273@163.com>
2020-01-06 17:01:53 +0800

    数据适配

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,4 +1,5 @@
-// pages/news/news.js
+var newsData = require("../data/newsdata.js");
+
 Page({
 
   /**
@@ -15,6 +16,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // console.log(newsData);
 
+    //this.setData可以让view重绘
+    this.setData({
+      useData:newsData.initData
+    })
   }
 })
