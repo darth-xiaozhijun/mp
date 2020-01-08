43d68de90bd6f7359ed34349c793fdea1dc3537e
darth-xiaozhijun <15717061273@163.com>
2020-01-08 21:31:54 +0800

    详情页

--- pages/news/news-detail/news-detail.js
+++ pages/news/news-detail/news-detail.js
@@ -12,6 +12,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(newsData.initData[options.newsid]);
+    // 页面初始化 options为页面跳转所带来的参数
+    this.setData(newsData.initData[options.newsid]);
+    // console.log(newsData.initData[options.newsid]);
+    newsid: options.newsid
   }
 })
